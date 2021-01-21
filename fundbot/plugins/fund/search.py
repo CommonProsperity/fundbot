94889a0f090a559fdd33d20b0747c11969a91812
fyc1007261 <caiyifan@seas.upenn.edu>
2021-01-21 09:46:06 +0800

    fix bot send interface

--- fundbot/plugins/fund/search.py
+++ fundbot/plugins/fund/search.py
@@ -18,7 +18,9 @@ search_cmd = on_command("search", rule=None, priority=5)
 @search_cmd.handle()
 async def handle_search(bot: Bot, event: Event, state: T_State):
     result = await search_impl.search_impl(str(event.get_message()))
-    await bot.send(event, result, False)
+    msg = Message(result)
+    print(msg)
+    await bot.send(event, msg, False)
 
 
 
