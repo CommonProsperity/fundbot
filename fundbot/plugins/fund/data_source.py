92e711c08a6dc909fd799306df2b6e65a9796067
jiahao_li <jiahao_li@trip.com>
2021-01-27 16:58:27 +0800

    add additional param to flag<fund> to set compared date

--- fundbot/plugins/fund/data_source.py
+++ fundbot/plugins/fund/data_source.py
@@ -2,9 +2,11 @@ import datetime
 from functools import wraps
 from typing import Union, List, Dict
 import traceback
+import asyncio
 
 import httpx
 from nonebot.log import logger, default_format
+from bs4 import BeautifulSoup
 
 from fundbot import util
 
@@ -23,6 +25,11 @@ def daily_cache(func):
             cached = res
         return cached
     return _wrapper
+
+
+async def make_request(client, url):
+    r = await client.get(url)
+    return r
 
 
 # @daily_cache
@@ -51,6 +58,37 @@ async def get_fund_data(fund_id: str) -> str:
             return "卧槽查不到啊"
 
 
+async def get_fund_trend_data(fund_id: str, start_date: str) ->  str:
+    recent_time = util.get_random_dt()
+    try:
+        start_time = util.get_timestamp_from_dt(start_date)
+    except ValueError:
+        logger.error(f"Error date format: {start_date}")
+        return "时间格式不对啊"
+    except:
+        traceback.print_exc()
+        logger.error(f"Date format: {start_date}")
+    if start_time >= recent_time:
+        return "你穿越回来得吗？"
+    start_date_url = f"https://fundf10.eastmoney.com/F10DataApi.aspx?type=lsjz&code={fund_id}&page=1&per=1&sdate={start_date}&edate={start_date}"
+    recent_date_url = f"https://fundgz.1234567.com.cn/js/{fund_id}.js?rt={recent_time}"
+    async with httpx.AsyncClient() as client:
+        task_list = []
+        recent_date_req = make_request(client, recent_date_url)
+        recent_date_task = asyncio.create_task(recent_date_req)
+        task_list.append(recent_date_task)
+        start_date_req = make_request(client, start_date_url)
+        start_date_task = asyncio.create_task(start_date_req)
+        task_list.append(start_date_task)
+        r = await asyncio.gather(*task_list)
+    if len(r) == 2 and r[0] and r[1]:
+        recent_data = decode_fund_data(r[0])
+        start_data = decode_fund_range_data(r[1])
+        return format_fund_data(fund_data=recent_data, compared_data=start_data)
+    else:
+        return "出问题了，兄弟"
+
+
 def decode_fund_data(fund_data: str) -> Dict:
     data = fund_data.text
     try:
@@ -64,7 +102,27 @@ def decode_fund_data(fund_data: str) -> Dict:
         logger.error(data)
 
 
-def format_fund_data(fund_data: Dict) -> str:
+def decode_fund_range_data(fund_range_data: str) -> Dict:
+    raw_data = fund_range_data.text
+    try:
+        if raw_data[0:22] == 'var apidata={ content:':
+            raw_data = raw_data[23:raw_data.find("records:")-2]
+        else:
+            logger.error("Not jsonpgz type")
+            return None
+        fund_data = {}
+        soup = BeautifulSoup(raw_data, 'html')
+        if len(soup.table.tbody.tr.select('td')) < 7:
+            return {"jzrq":"", "gsz":""}
+        fund_data["jzrq"] = soup.table.tbody.tr.select('td')[0].text
+        fund_data["gsz"] = soup.table.tbody.tr.select('td')[1].text
+        return fund_data
+    except:
+        traceback.print_exc()
+        logger.error(raw_data)
+
+
+def format_fund_data(fund_data: Dict, compared_data: Dict = None) -> str:
     # format output
     result = ""
     format_pattern = "%s: %s\n"
@@ -76,6 +134,11 @@ def format_fund_data(fund_data: Dict) -> str:
     result += format_pattern % ("估算净值", fund_data["gsz"])
     result += format_pattern % ("估算增值率", fund_data["gszzl"])
     result += format_pattern % ("估算时间", fund_data["gztime"])
+    if compared_data:
+        if not compared_data['jzrq'] or not compared_data['gsz']:
+            result += f"\n\n指定日期非交易日或数据已过期"
+        else:
+            result += format_pattern % (f"净值对比{compared_data['jzrq']}增长", format(float(fund_data["gsz"]) - float(compared_data["gsz"]), '.4f'))
     # delete last '\n'
     result = result[:-1]
     if float(fund_data['gszzl']) < 0:
--- fundbot/plugins/fund/fund.py
+++ fundbot/plugins/fund/fund.py
@@ -15,10 +15,15 @@ async def handle_fund(bot: Bot, event: Event, state: T_State):
     args = str(event.get_message()).split()
     result = ""
     if len(args) == 0:
-        await fund.finish("用法: #fund <fundid>")
-    elif len(args) >= 2:
+        await fund.finish("用法1: #fund <fundid> \n用法2: #fund <fundid> <compareddate(y-m-d)> ")
+    elif len(args) >= 3:
         result += "超过的参数会被忽略\n"
-    result = result + (await data_source.get_fund_data(args[0]))
+    # fund <fundid>
+    if len(args) == 1:
+        result = result + (await data_source.get_fund_data(args[0]))
+    # fund <fundid> <compareddate(y-m-d)>
+    elif len(args) == 2:
+        result = result + (await data_source.get_fund_trend_data(args[0], args[1]))
     msg = Message(result)
     print(msg)
     await bot.send(event, msg, False)
